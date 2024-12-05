65a839ce6d704b93a3f38c79eb52de1f11fd5af0
jambo <mijac0624@qq.com>
2024-12-05 12:40:29 +0000

    delete: useless code

--- internal/article/domain/repository/repository.go
+++ internal/article/domain/repository/repository.go
@@ -2,8 +2,6 @@ package repository
 
 import (
 	articleEntity "github.com/jambo0624/blog/internal/article/domain/entity"
-	categoryEntity "github.com/jambo0624/blog/internal/category/domain/entity"
-	tagEntity "github.com/jambo0624/blog/internal/tag/domain/entity"
 )
 
 type ArticleRepository interface {
@@ -12,20 +10,4 @@ type ArticleRepository interface {
 	Update(article *articleEntity.Article) error
 	Delete(id uint) error
 	FindAll() ([]*articleEntity.Article, error)
-}
-
-type CategoryRepository interface {
-	Save(category *categoryEntity.Category) error
-	FindByID(id uint) (*categoryEntity.Category, error)
-	FindBySlug(slug string) (*categoryEntity.Category, error)
-	Update(category *categoryEntity.Category) error
-	Delete(id uint) error
-}
-
-type TagRepository interface {
-	Save(tag *tagEntity.Tag) error
-	FindByID(id uint) (*tagEntity.Tag, error)
-	FindByName(name string) (*tagEntity.Tag, error)
-	Update(tag *tagEntity.Tag) error
-	Delete(id uint) error
 }
