4ed1748c6a560cdae23eeebe1d9ad9065b8ade40
jambo <mijac0624@qq.com>
2024-12-07 13:42:33 +0000

    refactor: database

--- database/init.sql
+++ database/init.sql
@@ -1,8 +1,8 @@
--- 创建数据库
+-- create database
 CREATE DATABASE IF NOT EXISTS blog;
 CREATE DATABASE IF NOT EXISTS blog_test;
 
--- 创建用户并授权
+-- create user and grant privileges
 CREATE USER IF NOT EXISTS 'blog_user'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON blog.* TO 'blog_user'@'localhost';
 GRANT ALL PRIVILEGES ON blog_test.* TO 'blog_user'@'localhost';
